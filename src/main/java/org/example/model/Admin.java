2677b02594894295ad61795add431a659dc80d5b
Daniel Augusto <dan.saugusto@proton.me>
2026-07-26 20:48:31 -0300

    fix: adicionando o papel-enum para o admin

--- src/main/java/org/example/model/Admin.java
+++ src/main/java/org/example/model/Admin.java
@@ -11,7 +11,7 @@ import lombok.ToString;
 @Entity
 @DiscriminatorValue("ADMIN")
 public class Admin extends User {
-    private Role role;
+    private Role role = Role.ADMIN;
 
     public Admin() {
     }
