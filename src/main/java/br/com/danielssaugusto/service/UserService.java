807b3f5a3d78ce7f558843c210c98ae6d50054d9
Daniel Augusto <dan.saugusto@proton.me>
2026-07-27 19:04:41 -0300

    feature: instanciando o repositório user e funções na classe de serviço.

--- src/main/java/br/com/danielssaugusto/service/UserService.java
+++ src/main/java/br/com/danielssaugusto/service/UserService.java
@@ -1,4 +1,19 @@
 package br.com.danielssaugusto.service;
 
+import br.com.danielssaugusto.entity.User;
+import br.com.danielssaugusto.repository.UserRepository;
+
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createNewUser(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new IllegalArgumentException("User already exists.");
+        }
+        return userRepository.save(user);
+    }
 }
