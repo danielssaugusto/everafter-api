085a6666c07f069a822ba288a2f44998f3c2e350
Daniel Augusto <dan.saugusto@proton.me>
2026-07-27 16:57:36 -0300

    refactor: criaiação das entidades do sistema

--- src/main/java/org/example/repository/ProfessionalRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.repository;
-
-import org.example.model.Professional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfessionalRepository extends JpaRepository<Professional, Long> {
-    boolean existsByEmail(String email);
-    Professional findByEmail(String email);
-}
