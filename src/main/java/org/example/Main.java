4c351f115ec3045f58bd249d69a59eb737c801af
Daniel Augusto <dan.saugusto@proton.me>
2026-07-26 22:32:32 -0300

    refactor: alteração de lógica, exclusÃão do conceito de admin

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,5 @@
 package org.example;
 
-import org.example.model.Admin;
-import org.example.repository.AdminRepository;
-import org.example.service.AdminService;
-
-import java.sql.SQLOutput;
-import java.util.Scanner;
-
 public class Main {
     static void main(String[] args) {
     }
--- src/main/java/org/example/repository/GuestRepository.java
+++ src/main/java/org/example/repository/GuestRepository.java
@@ -1,7 +1,7 @@
 package org.example.repository;
 
 import org.example.model.Guest;
-import org.example.model.RSVPStatus;
+import org.example.enums.RSVPStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
