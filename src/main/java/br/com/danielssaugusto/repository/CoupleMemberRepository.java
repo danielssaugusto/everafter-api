eaa811e091a253e6efbb3843ed0bc1a997f7f05b
Daniel Augusto <dan.saugusto@proton.me>
2026-07-29 17:32:21 -0300

    fix: passando os metodos de novo cadastro para o membro do casal

--- src/main/java/br/com/danielssaugusto/repository/CoupleMemberRepository.java
+++ src/main/java/br/com/danielssaugusto/repository/CoupleMemberRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface CoupleMemberRepository extends JpaRepository<CoupleMember, UUID> {
+    boolean existsByEmail(String email);
 
 }
--- src/main/java/br/com/danielssaugusto/service/CoupleMemberService.java
+++ src/main/java/br/com/danielssaugusto/service/CoupleMemberService.java
@@ -1,34 +1,54 @@
 package br.com.danielssaugusto.service;
 
 import br.com.danielssaugusto.entity.CoupleMember;
+import br.com.danielssaugusto.entity.User;
 import br.com.danielssaugusto.repository.CoupleMemberRepository;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Service
 public class CoupleMemberService {
     CoupleMemberRepository coupleMemberRepository;
-    Optional<CoupleMember> coupleMember;
+    CoupleMember coupleMember;
 
     @Transactional
-    public Optional<CoupleMember> updateCoupleMemberEmail(UUID id, String email) {
-        coupleMember = Optional.of(coupleMemberRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Couple member not found.")));
+    public CoupleMember signUpNewCoupleMember(CoupleMember coupleMember) {
 
-        coupleMember.get().setEmail(email);
+        if (coupleMemberRepository.existsByEmail(coupleMember.getEmail())) {
+            throw new IllegalArgumentException("User already exists.");
+        }
 
+        return coupleMemberRepository.save(coupleMember);
+    }
+
+    public User singInCoupleMember(CoupleMember coupleMember) {
+        if (!coupleMemberRepository.existsByEmail(coupleMember.getEmail())) {
+            throw new IllegalArgumentException("User not found.");
+        }
         return coupleMember;
     }
 
     @Transactional
-    public Optional<CoupleMember> updateCoupleMemberPassword(UUID id, String password) {
-        coupleMember = Optional.of(coupleMemberRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Couple member not found")));
-        coupleMember.get().setEmail(password);
+    public CoupleMember updateCoupleMemberEmail(UUID id, String email) {
 
-        return coupleMember;
+        CoupleMember member = coupleMemberRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Couple member not found."));
+
+        member.setEmail(email);
+
+        return coupleMemberRepository.save(member);
     }
 
+    @Transactional
+    public CoupleMember updateCoupleMemberPassword(UUID id, String password) {
+        coupleMember = coupleMemberRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Couple member not found"));
 
+        coupleMember.setPassword(password);
+
+        return coupleMemberRepository.save(coupleMember);
+    }
 }
