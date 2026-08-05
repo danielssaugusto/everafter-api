c53dc026d36e683148045c56fc97aeac814a32db
Daniel Augusto <dan.saugusto@proton.me>
2026-08-05 12:02:52 -0300

    feat: adicionando um get geral e um post com segurança

--- src/main/java/br/com/danielssaugusto/controller/CoupleMemberRestController.java
+++ src/main/java/br/com/danielssaugusto/controller/CoupleMemberRestController.java
@@ -1,12 +1,13 @@
 package br.com.danielssaugusto.controller;
 
+import br.com.danielssaugusto.dto.CoupleMemberRequestDTO;
 import br.com.danielssaugusto.dto.CoupleMemberResponseDTO;
 import br.com.danielssaugusto.entity.Couple;
 import br.com.danielssaugusto.entity.CoupleMember;
 import br.com.danielssaugusto.repository.CoupleMemberRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.concurrent.CountedCompleter;
@@ -24,5 +25,16 @@ public class CoupleMemberRestController {
     @GetMapping
     public List<CoupleMemberResponseDTO> listar() {
         return coupleMemberRepository.findAll().stream().map(CoupleMemberResponseDTO::new).collect(Collectors.toList());
+    }
+
+    @PostMapping("/protect")
+    public ResponseEntity<CoupleMemberResponseDTO> createWitchProtect(@RequestBody CoupleMemberRequestDTO dto) {
+        CoupleMember newMember = new CoupleMember(
+                dto.getName(),
+                dto.getEmail(),
+                dto.getPassword()
+        );
+        CoupleMember saved = coupleMemberRepository.save(newMember);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new CoupleMemberResponseDTO(saved));
     }
 }
