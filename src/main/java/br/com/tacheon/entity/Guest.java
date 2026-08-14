631f7b934cdeb2e5443bc19e89db7ce8f44ae4a8
Daniel Augusto <dan.saugusto@proton.me>
2026-08-14 16:49:26 -0300

    feat: create atributes to some entities and create enums for them

--- src/main/java/br/com/tacheon/entity/Guest.java
+++ src/main/java/br/com/tacheon/entity/Guest.java
@@ -1,4 +1,31 @@
 package br.com.tacheon.entity;
 
+import br.com.tacheon.enums.AgeClassification;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class Guest {
+    private UUID guestID;
+    private Family familyId;
+    private String guestName;
+    private String phone;
+    private AgeClassification guestType;
+    private LocalDate initDate;
+    private LocalDate updateDate;
+
+    public Guest(
+            Family familyId,
+            String guestName,
+            String phone,
+            AgeClassification guestType,
+            LocalDate initDate,
+            LocalDate updateDate) {
+        this.familyId = familyId;
+        this.guestName = guestName;
+        this.phone = phone;
+        this.guestType = guestType;
+        this.initDate = initDate;
+        this.updateDate = updateDate;
+    }
 }
