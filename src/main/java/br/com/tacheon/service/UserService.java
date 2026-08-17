b1d4fb7767a99d06f50d0e67794a51229e8e7cbe
Daniel Augusto <dan.saugusto@proton.me>
2026-08-17 18:38:02 -0300

    feat: create register user function

--- src/main/java/br/com/tacheon/service/UserService.java
+++ src/main/java/br/com/tacheon/service/UserService.java
@@ -1,5 +1,14 @@
 package br.com.tacheon.service;
 
+import br.com.tacheon.entity.User;
+import br.com.tacheon.repository.UserRepository;
+
 public class UserService {
+    UserRepository userRepository;
+
+    User registerUser(User user) {
+        userRepository.save(user);
+        return user;
+    }
 
 }
