ea0b71ccb5799fc1bd48151cdaa55b329725e983
Daniel Augusto <dan.saugusto@proton.me>
2026-08-19 12:26:19 -0300

    feat: create CRUD to invite, in progress

--- src/main/java/br/com/tacheon/services/InviteService.java
+++ src/main/java/br/com/tacheon/services/InviteService.java
@@ -1,4 +1,36 @@
 package br.com.tacheon.services;
 
+import br.com.tacheon.entities.Invite;
+import br.com.tacheon.repositories.InviteRepository;
+
+import java.util.UUID;
+
 public class InviteService {
+    private final InviteRepository inviteRepository;
+
+    public InviteService(InviteRepository inviteRepository) {
+        this.inviteRepository = inviteRepository;
+    }
+
+    private final Invite findInvite(UUID id) {
+        Invite invite = inviteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Invite not found."));
+
+        return invite;
+    }
+
+    // create invite
+    public Invite createInvite(UUID inviteId) {
+        Invite invite = findInvite(inviteId);
+
+        return inviteRepository.save(invite);
+    }
+
+    // send invite
+
+    // cancel invite
+
+    // search invite
+
+
 }
